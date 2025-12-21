e97b807b1c568534d2fcb0f3b2bb6e0a1ba771c4
Kamal Aboumoustafa <kamalabomostafa2@gmail.com>
2025-12-20 18:33:41 -0800

    Fix: Convert vite.config.js to ESM for Netlify build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-const { resolve } = require('path')
-const { defineConfig } = require('vite')
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
 
-module.exports = defineConfig({
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export default defineConfig({
   build: {
     rollupOptions: {
       input: {
